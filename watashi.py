1298bdde1d46ec461c447c2325325e29bc7ce0ff
James <naughtsee@gmail.com>
2017-10-16 16:18:31 -0400

    Sublcass bot & add a launcher
    PEP8'd some variable names & made others more descriptive
    Also removed some imports that weren't necessary (yet)

--- watashi.py
+++ watashi.py
@@ -1,27 +1,34 @@
 import discord
 from discord.ext import commands
-import asyncio
+# import asyncio
 import json
-import io
-import random
-import time
-import random
+# import io
+# import random
+# import time
+# import random
 import aiohttp
 
-serverPrefix = {***REMOVED***
 
-basecogs = ('cogs.base')
-#Prefix not yet setup. Make config file.
-bot = commands.Bot(command_prefix=, self_bot=True)
-bot.aio_session = aiohttp.ClientSession(loop=bot.loop)
+class Watashi(commands.Bot):
+    def __init__(self, config_path: str, *args, **kwargs):
+        self.config_path = config_path
+        with open(self.config_path) as f:
+            self.configs = json.load(f)
 
-@bot.event
-async def on_ready():
-	print("Bot Online!")
-	print("Name: {***REMOVED***".format(bot.user.name))
-	print("ID: {***REMOVED***".format(bot.user.id))
-	print(discord.__version__)
-	for x in basecogs:
-		bot.load_extension(x)
+        self.aiohttp_session = aiohttp.ClientSession(loop=self.loop)
 
-bot.run("", bot=False)
+        self.default_cogs = ('cogs.base')
+
+        super().__init__(command_prefix=configs['prefix'], self_bot=True) 
+
+    async def run(self):
+        super().run(configs['bot_token'], bot=False)
+
+    async def on_ready(self):
+    	print('Bot Online!')
+    	print(f'Name: {bot.user.name***REMOVED***')
+    	print(f'ID: {bot.user.id***REMOVED***')
+    	print(f'Discord.py Version: {discord.__version__***REMOVED***')
+
+    	for cog in self.default_cogs:
+    		bot.load_extension(cog)
