112eb4d0dbf7ce84a22330c449355747d3017c2b
Rohan Radia <Rohan@rohans-mbp.home>
2017-10-17 20:31:52 +0100

    New message on start up

--- watashi.py
+++ watashi.py
@@ -17,6 +17,8 @@ class Watashi(commands.Bot):
         
         self.default_cogs = ('cogs.base', 'cogs.coding')
         
+        self.embed_colour = int(self.configs['embed_colour'], 16)
+
         super().__init__(command_prefix=self.configs['prefix'], self_bot=True) 
 
         self.aiohttp_session = aiohttp.ClientSession(loop=self.loop)
@@ -25,8 +27,7 @@ class Watashi(commands.Bot):
         super().run(self.configs['token'], bot=False)
 
     async def on_ready(self):
-            print("Welcome to Watashi SelfBot!")
-            print("Using this bot is against the Discord TOS.")
-            print("Use at your own risk")
+            print('\n'*100)
+            print("<----------------->\nWatashi SelfBot\n<----------------->\nCoded by:\nPrestige#9162\nDemo#9465\nnaught0#4417\n<----------------->\nWarning:\nUnder the MIT\nlisense we are\nnot liable for any\ndamage casued/\naction taken\nagainst you for\nusing a selfbot\nwhich is in violation\nof Discord's TOS")
             for cog in self.default_cogs:
                 self.load_extension(cog)
