da51c052c16ec0051ef7ad10cfe5e1318a1ee09b
Rohan Radia <Rohan@rohans-mbp.home>
2017-10-17 21:32:18 +0100

    New Status Command

--- cogs/base.py
+++ cogs/base.py
@@ -20,8 +20,23 @@ class Base:
 			await ctx.message.delete()
 			await ctx.send(":x: You need a message to embed")
 		else:
+			await ctx.message.delete()
 			emb = discord.Embed(title=message, colour=self.bot.embed_colour)
 			await ctx.send(embed=emb)
+
+	@commands.command(hidden=True, aliases=['status'])
+	async def presense(self, ctx, mode, *, message: str = None):
+		if mode.lower() == "stream" or mode.lower() == "twitch":
+			await self.bot.change_presence(status=discord.Status.online, game=discord.Game(name=message,type=1,url="https://www.twitch.tv/{***REMOVED***".format(message)), afk=True)
+		elif mode.lower() == "online" or mode.lower() == "on":
+			await self.bot.change_presence(status=discord.Status.online, game=discord.Game(name=message), afk=True)
+		elif mode.lower() == "idle":
+			await self.bot.change_presence(status=discord.Status.idle, game=discord.Game(name=message), afk=True)
+		elif mode.lower() == "dnd" or mode.lower() == "disturb" or mode.lower() == "donotdisturb":
+			await self.bot.change_presence(status=discord.Status.dnd, game=discord.Game(name=message), afk=True)
+		elif mode.lower() == "invisible" or mode.lower() == "invis":
+			await self.bot.change_presence(status=discord.Status.invisible, game=discord.Game(name=message), afk=True)
+		elif mode.lower() == "none" or mode.lower() == "clear":
 
 def setup(bot):
 	bot.add_cog(Base(bot))
