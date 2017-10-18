54a4a6e4bc4fda39210fad4c615f6c8f2451ecf5
Rohan Radia <Rohan@rohans-mbp.home>
2017-10-18 18:11:08 +0100

    Fix

--- cogs/info.py
+++ cogs/info.py
@@ -24,6 +24,21 @@ class Info:
 				await ctx.send(embed=emb)
 			except:
 				await ctx.send("Too much info...")
+		else:
+			emb = discord.Embed(colour=0xC500FF)
+			emb.set_author(name="Whois for {***REMOVED***".format(member.display_name),
+							icon_url=member.avatar_url)
+			emb.set_thumbnail(url=member.avatar_url)
+			emb.add_field(name="**ID**", value=member.id)
+			emb.add_field(name="**Roles**", value=", ".join([r.name for r in member.roles]))
+			emb.add_field(name="**Status**", value="**Playing** {***REMOVED***".format(member.game.name if member.game else ""))
+			emb.add_field(name="**Color**", value=str(member.color))
+			emb.add_field(name="**Joined on**", value=member.joined_at.date())
+			emb.add_field(name="**Avatar url**", value="[Here]({***REMOVED***)".format(member.avatar_url))
+			try:
+				await ctx.send(embed=emb)
+			except:
+				await ctx.send("Too much info...")
 
 def setup(bot):
 	bot.add_cog(Info(bot))
