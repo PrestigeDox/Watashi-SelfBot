06bdddc2e4942c93e5bc186ade06ba3c5c04bb2b
Rohan Radia <Rohan@Rohans-MacBook-Pro.local>
2017-10-19 10:56:27 +0100

    New text

---
+++ cogs/figlet.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import pyfiglet
+
+class Figlet:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=['ascii', 'fig', 'asc'])
+    async def figlet(self, ctx, *, message: str):
+        await ctx.message.delete()
+        try:
+            await ctx.send(f'```http\n{pyfiglet.figlet_format(message)***REMOVED***```')
+        except discord.HTTPException:
+            pass
+
+def setup(bot):
+    bot.add_cog(Figlet(bot))
