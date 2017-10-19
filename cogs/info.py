11fb889c7abc1272544b92bbf1a45241efbd605e
Rohan Radia <Rohan@Rohans-MacBook-Pro.local>
2017-10-19 15:11:45 +0100

    Added avatar

--- cogs/info.py
+++ cogs/info.py
@@ -6,7 +6,7 @@ class Info:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(hidden=True, aliases=['info'])
+    @commands.command(aliases=['info'])
     async def whois(self, ctx, member: discord.Member = None):
         if member == None:
             memberholder = ctx.message.author.id
@@ -43,6 +43,16 @@ class Info:
         except:
             await ctx.send("Too much info...")
 
+    @commands.command()
+    async def avatar(self, ctx, member: discord.Member = None):
+        await ctx.message.delete()
+        emb = discord.Embed(colour=self.bot.embed_colour)
+        emb.set_author(name="Avatar for {***REMOVED***".format(member.display_name),
+                           icon_url=member.avatar_url)
+        emb.add_field(name="**Avatar url**",
+                          value="[Here]({***REMOVED***)".format(member.avatar_url))
+        emb.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=emb)
 
 def setup(bot):
     bot.add_cog(Info(bot))
