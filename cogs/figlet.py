7004bd1a90f8ee7d4be96ece0f67b09acce430f8
Rohan Radia <Rohan@Rohans-MacBook-Pro.local>
2017-10-19 11:42:43 +0100

    New Fonts x2

--- cogs/figlet.py
+++ cogs/figlet.py
@@ -16,5 +16,38 @@ class Figlet:
             emb.add_field(name=":x: Error", value="Message is too large!")
             await ctx.send(embed=emb)
 
+    @commands.command(aliases=['asciislant', 'figslant', 'ascslant'])
+    async def figletslant(self, ctx, *, message: str):
+        await ctx.message.delete()
+        f = pyfiglet.Figlet(font='slant')
+        try:
+            await ctx.send(f'```http\n{f.renderText(message)}```')
+        except discord.HTTPException:
+            emb = discord.Embed(colour=self.bot.red)
+            emb.add_field(name=":x: Error", value="Message is too large!")
+            await ctx.send(embed=emb)
+
+    @commands.command(aliases=['asciibulbhead', 'figbulbhead', 'ascbulbhead'])
+    async def figletbulbhead(self, ctx, *, message: str):
+        await ctx.message.delete()
+        f = pyfiglet.Figlet(font='bulbhead')
+        try:
+            await ctx.send(f'```http\n{f.renderText(message)}```')
+        except discord.HTTPException:
+            emb = discord.Embed(colour=self.bot.red)
+            emb.add_field(name=":x: Error", value="Message is too large!")
+            await ctx.send(embed=emb)
+
+    @commands.command(aliases=['ascii3d', 'fig3d', 'asc3d'])
+    async def figlet3d(self, ctx, *, message: str):
+        await ctx.message.delete()
+        f = pyfiglet.Figlet(font='larry3d')
+        try:
+            await ctx.send(f'```http\n{f.renderText(message)}```')
+        except discord.HTTPException:
+            emb = discord.Embed(colour=self.bot.red)
+            emb.add_field(name=":x: Error", value="Message is too large!")
+            await ctx.send(embed=emb)
+
 def setup(bot):
     bot.add_cog(Figlet(bot))
