066511c7db3af4e14a2b602caa4d3b9499289fa8
Rohan Radia <Rohan@Rohans-MacBook-Pro.local>
2017-10-19 13:46:49 +0100

    Added INI

--- cogs/coding.py
+++ cogs/coding.py
@@ -22,6 +22,12 @@ class Coding:
         await ctx.send("```html\n{***REMOVED***```".format(code))
 
     @commands.command()
+    async def ini(self, ctx, *, code: str):
+        await ctx.message.delete()
+        await ctx.send("```ini\n{***REMOVED***```".format(code))
+
+
+    @commands.command()
     async def c(self, ctx, *, code: str):
         await ctx.message.delete()
         await ctx.send("```c\n{***REMOVED***```".format(code))
