67b5da6f590451aa397821a43156c198f1cae4b5
Rohan Radia <Rohan@rohans-mbp.home>
2017-10-19 17:54:37 +0100

    Begining Work on Info

--- cogs/info.py
+++ cogs/info.py
@@ -6,7 +6,7 @@ class Info:
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(aliases=['info'])
+    @commands.command()
     async def whois(self, ctx, member: discord.Member = None):
         if member == None:
             memberholder = ctx.message.author.id
@@ -52,6 +52,28 @@ class Info:
         emb.add_field(name="**Avatar url**",
                           value="[Here]({***REMOVED***)".format(member.avatar_url))
         emb.set_thumbnail(url=member.avatar_url)
+        await ctx.send(embed=emb)
+
+    @commands.command(aliases=['about', 'selfbot', 'bot'])
+    async def info(self, ctx):
+
+        github = '[Click Here](https://github.com/PrestigeDox/Watashi-SelfBot)'
+        discord = '[Click Here](https://discord.gg/JAcAEU5)'
+
+        await ctx.message.delete()
+        emb = discord.Embed(colour=self.bot.embed_colour)
+        emb.set_author(name="Watashi SelfBot", icon_url=member.avatar_url)
+        emb.add_field(name="About", value="Bot Description", inline=False)
+        emb.add_field(name="Uptime", value="Time Here", inline=False)
+        emb.add_field(name="Ping Time", value="Ping Time", inline=True)
+        emb.add_field(name="Users", value="Users Between Servers", inline=True)
+        emb.add_field(name="Servers", value="Servers your in", inline=True)
+        emb.add_field(name="Text Channels", value="Amount of Text Channels", inline=True)
+        emb.add_field(name="Voice Channels", value="Amount of Voice Channels", inline=True)
+        emb.add_field(name="Presence", value="Your Presence", inline=True)
+        emb.add_field(name="Playing", value="What Your Playing", inline=True)
+        emb.add_field(name="GitHub", value=github, inline=True)
+        emb.add_field(name="Discord", value=discord, inline=True)
         await ctx.send(embed=emb)
 
 def setup(bot):
