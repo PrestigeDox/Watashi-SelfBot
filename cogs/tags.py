6a02864b9aadbec9be63b599803d4946e6760a81
James <naughtsee@gmail.com>
2017-10-20 16:45:59 -0400

    untested tag command

---
+++ cogs/tags.py
@@ -0,0 +1,90 @@
+# !/bin/env python3
+import json
+from pathlib import Path
+from discord.ext import commands
+
+class Tag:
+    def __init__(self, bot):
+        self.bot = bot
+        self._load_tag_file()
+
+    @staticmethod
+    def _tag_file_exists():
+        """ Checks to see if there is a tag file in the proper place """
+        return Path('tag_file.json').is_file()
+
+    @staticmethod
+    def _tag_file_valid():
+        """ Ensures proper json file is found """
+        try:
+            with open('tag_file.json') as f:
+                json.load(f)
+        except:
+            return False
+
+        return True
+
+    def _load_tag_file(self):
+        """ Loads the tag file as a class attr """
+        if self._tag_file_exists() and self._tag_file_valid():
+            with open('tag_file.json') as f:
+                self.tag_dict = json.load(f)
+        else:
+            self.tag_dict = {}
+            raise Exception(IOError, 'Tag file does not exist or does not contain valid JSON.')
+
+    def _write_tag_file(self):
+        """ Writes the content of the tag_dict to the file """
+        try:
+            with open('tag_file.json', 'w') as f:
+                json.dump(self.tag_dict, f)
+        except Exception as e:
+            print(f'Problem writing to tag file\n{e}')
+
+    @commands.group(invoke_without_command=True)
+    async def tag(self, ctx, *, tag_name: str):
+        """ Retrieve a previously stored tag """
+        if tag_name in self.tag_dict:
+            return await ctx.send(self.tag_dict[tag_name])
+
+        await ctx.send(f'Tag `{tag_name}` does not exist.', delete_after=10.0)
+
+    @commands.tag()
+    async def create(self, ctx, tag_name: str, *, tag_contents: str):
+        """ Create a new tag """
+        if tag_name in self.tag_dict:
+            return await ctx.send(f'Tag `{tag_name} already exists. Use `tag edit` to change it.', delete_after=10.0)
+
+        self.tag_dict[tag_name] = tag_contents
+        # Not sure if this is necessary but it feels right, man
+        self._write_tag_file()
+        self._load_tag_file()
+
+        await ctx.send(f'Tag `{tag_name}` successfully created.', delete_after=10.0)
+
+    @commands.tag(name='delete', aliases='del')
+    async def _delete(self, ctx, *, tag_name: str):
+        """ Delete a tag you've previously created """
+        if tag_name not in self.tag_dict:
+            return await ctx.send(f'Tag `{tag_name}` does not exist.', delete_after=10.0)
+
+        del self.tag_dict[tag_name]
+        self._write_tag_file()
+        self._load_tag_file()
+
+        await ctx.send(f'Tag `{tag_name}` deleted.', delete_after=10.0)
+
+    @commands.tag()
+    async def edit(self, ctx, tag_name: str, *, tag_contents: str):
+        """ Edit a tag which you've previously created """
+        if tag_name not in self.tag_dict:
+            return await ctx.send(f'Tag `{tag_name}` does not exist.', delete_after=10.0)
+
+        self.tag_dict[tag_name] = tag_contents
+        self._write_tag_file()
+        self._load_tag_file()
+
+        await ctx.send(f'Tag `{tag_name}` succesfully edited.', delete_after=10.0)
+
+def setup(bot):
+    bot.add_cog(Tag(bot))
