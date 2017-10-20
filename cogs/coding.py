84c1557485f1ef7fe9d88542895596cfc48fa2be
Rohan Radia <Rohan@Rohans-MacBook-Pro.local>
2017-10-20 18:23:43 +0100

    Added CharINFO

--- cogs/coding.py
+++ cogs/coding.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import unicodedata
 
 
 class Coding:
@@ -42,6 +43,20 @@ class Coding:
         await ctx.message.delete()
         await ctx.send("``\n{}```".format(code))
 
+    @commands.command()
+    async def charinfo(self, ctx, *, characters: str):
+        if len(characters) > 25:
+            return await ctx.send(f'Too many characters ({len(characters)}/25)')
+
+        digit = f'{ord(characters):x}'
+        name = unicodedata.name(characters, 'Name not found.')
+        
+        emb = discord.Embed(colour=self.bot.embed_colour)
+        emb.set_author(name="Charinfo For {}".format(characters))
+        emb.add_field(name="Name", value=name, inline=False)
+        emb.add_field(name="Char", value=f'\\U{digit:>08}', inline=False)
+        emb.add_field(name="Link", value=f'<http://www.fileformat.info/info/unicode/char/{digit}>')
+        await ctx.send(embed=emb)
 
 def setup(bot):
     bot.add_cog(Coding(bot))
