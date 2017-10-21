2f76f39c99db14d364a276965afea35a8b27c146
James <naughtsee@gmail.com>
2017-10-21 15:14:53 -0400

    add error cog

---
+++ cogs/error.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+
+class ErrorFormatter:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='error', hidden=True)
+    async def error_formatter(self, ctx, *, err: str):
+        """ Send a nice embed error with a message
+        Usage: In other cogs
+        cmd = bot.get_command('error')
+        await ctx.invoke(cmd, 'error message here')
+        """
+        em = discord.Embed(title=':x: Error', color=discord.Color.red(), description=err)
+        await ctx.send(embed=em)
+
+
+def setup(bot):
+    bot.add_cog(ErrorFormatter(bot))
