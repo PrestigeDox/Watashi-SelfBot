ad1126d10cc836907d158bf9e6d43a4554c7a793
James <naughtsee@gmail.com>
2017-10-21 15:49:42 -0400

    docstring

--- cogs/error.py
+++ cogs/error.py
@@ -8,9 +8,17 @@ class ErrorFormatter:
     @commands.command(name='error', hidden=True)
     async def error_formatter(self, ctx, *, err: str):
         """ Send a nice embed error with a message
-        Usage: In other cogs
-        cmd = bot.get_command('error')
-        await ctx.invoke(cmd, err='error message here')
+
+        Usage:
+        class Foo:
+            def __init__(self, bot):
+                self.bot = bot
+                self.cmd = bot.get_command('error')
+
+        @commands.command()
+        async def bar(self, ctx, *, baz):
+            # some error happened
+            await ctx.invoke(cmd, err='error message here')
         """
         em = discord.Embed(title=':x: Error', color=discord.Color.dark_red(), description=err)
         await ctx.send(embed=em)
