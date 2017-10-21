521442ee6265de0a6ef1b0e1d9c85b17888bd27f
James <naughtsee@gmail.com>
2017-10-20 20:49:01 -0400

    embed color added

--- cogs/tags.py
+++ cogs/tags.py
@@ -114,7 +114,8 @@ class Tag:
         total_tags = len(self.tag_dict)
         total_tag_uses = sum(x['uses'] for x in self.tag_dict.values())
         em = discord.Embed(title='Tag Statistics', description=f'Total tags: {total_tags}\n'
-                                                               f'Total tag uses: {total_tag_uses}')
+                                                               f'Total tag uses: {total_tag_uses}',
+                                                    color=self.bot.embed_colour)
 
         ranked_tag_list = sorted(self.tag_dict, key=lambda x: self.tag_dict[x]['uses'], reverse=True)
         ranked_tag_list_str = '\n'.join([f'{idx+1}\U000020e3 {x} ({self.tag_dict[x]["uses"]} uses)' for idx, x in enumerate(ranked_tag_list)])
