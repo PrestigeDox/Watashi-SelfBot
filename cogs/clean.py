dd11de2b3010c607399501ff11255f384da93aaf
Rohan Radia <Rohan@rohans-mbp.home>
2017-10-22 11:17:30 +0100

    Space Tab Error

--- cogs/clean.py
+++ cogs/clean.py
@@ -9,14 +9,13 @@ class Clean:
 
     @commands.command(aliases=['clear'])
     async def clean(self, ctx, *, limit = None):
-    	await ctx.message.delete()
+        await ctx.message.delete()
         if not limit:
             limit = 30
         async for msg in ctx.channel.history(limit=limit, before=ctx.message):
-        	if msg.author == self.bot.user:
-        		await msg.delete()
-        		await asyncio.sleep(1.2)
-
+            if msg.author == self.bot.user:
+                await msg.delete()
+                await asyncio.sleep(1.2)
 
 def setup(bot):
     bot.add_cog(Clean(bot))
