5f80db73911ea39814a1d2cd44a9d4c3a57c3fd9
James <naughtsee@gmail.com>
2017-10-23 20:34:20 -0400

    add option to delete message upon error

--- cogs/error.py
+++ cogs/error.py
@@ -7,7 +7,7 @@ class ErrorFormatter:
         self.bot = bot
 
     @commands.command(name='error', hidden=True)
-    async def error_formatter(self, ctx, delete_after=10.0, err: str=None):
+    async def error_formatter(self, ctx, delete_after=10.0, err: str=None, del_msg: discord.Message=None):
         """ Send a nice embed error with a message
 
         Usage:
@@ -18,8 +18,13 @@ class ErrorFormatter:
             @commands.command()
             async def bar(self, ctx, *, baz):
                 # some error happened
-                await ctx.invoke(self.bot.get_command('error'), delete_after=1.0, err='error message here')
+                await ctx.invoke(self.bot.get_command('error'),
+                                 del_msg=ctx.message, delete_after=1.0,
+                                 err='error message here')
         """
+        if del_msg is not None:
+            await del_msg.delete()
+
         em = discord.Embed(title=':x: Error', color=discord.Color.dark_red(), description=err)
         await ctx.send(embed=em, delete_after=delete_after)
 
