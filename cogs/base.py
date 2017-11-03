03cb47555bbf96ea8cb6976926185186e79d33ed
Ary Y <gtownx36@gmail.com>
2017-11-03 22:22:56 +0530

    Some quote fixes, also comments, its nothing complex, but still

--- cogs/base.py
+++ cogs/base.py
@@ -95,10 +95,13 @@ class Base:
     async def quote(self, ctx, message_id: int = None, *, reply: str = None):
         """ Quote a message by using its ID """
 
+        # Check for message_id, if it wasn't passed, return
         if message_id is None:
             return await ctx.error('Provide a message id please')
 
+        # Just for the sake of good UX, im giving each error its own except block and message
         try:
+            # Credit to Spy for this, its quite clever
             obj = discord.Object(id=message_id + 1)
             async for mess in ctx.channel.history(limit=1, before=obj):
                 if mess.id == message_id:
@@ -115,26 +118,35 @@ class Base:
         emb = discord.Embed(colour=self.color, description=message.content)
         emb.set_author(name=f'{message.author.display_name}#{message.author.discriminator}',
                        icon_url=message.author.avatar_url)
-        emb.set_footer(text=f'#{ctx.channel.name} | {message.created_at.strftime("%a, %d %b %Y at %I:%M%p")}')
+
+        # DMChannel doesn't have a name attr, not doing any fancy ternary op, its already messy
+        if not isinstance(ctx.channel, discord.DMChannel):
+            emb.set_footer(text=f'#{ctx.channel.name} | {message.created_at.strftime("%a, %d %b %Y at %I:%M%p")}')
+        else:
+            emb.set_footer(text=f'{message.created_at.strftime("%a, %d %b %Y at %I:%M%p")}')
 
         await ctx.message.delete()
 
         await ctx.send(embed=emb)
 
+        # Optional reply
         if reply is not None:
             await ctx.send(reply)
 
     @quote.command(name='fake')
     async def fake(self, ctx, message_id: int = None, *, fake_text: str = None):
         """ Make a fake quote with custom text referring to a real message by ID """
-
+        # Check for message_id, if it wasn't passed, return
         if message_id is None:
             return await ctx.error('Provide a message id please')
 
+        # Fake quote needs fake text, if you didn't provide it why were you using this
         if fake_text is None:
             return await ctx.error('Provide text to substitute please')
 
+        # Just for the sake of good UX, im giving each error its own except block and message
         try:
+            # Credit to Spy for this, its quite clever
             obj = discord.Object(id=message_id + 1)
             async for mess in ctx.channel.history(limit=1, before=obj):
                 if mess.id == message_id:
@@ -151,7 +163,12 @@ class Base:
         emb = discord.Embed(colour=self.color, description=fake_text)
         emb.set_author(name=f'{message.author.display_name}#{message.author.discriminator}',
                        icon_url=message.author.avatar_url)
-        emb.set_footer(text=f'#{ctx.channel.name} | {message.created_at.strftime("%a, %d %b %Y at %I:%M%p")}')
+
+        # DMChannel doesn't have a name attr, not doing any fancy ternary op, its already messy
+        if not isinstance(ctx.channel, discord.DMChannel):
+            emb.set_footer(text=f'#{ctx.channel.name} | {message.created_at.strftime("%a, %d %b %Y at %I:%M%p")}')
+        else:
+            emb.set_footer(text=f'{message.created_at.strftime("%a, %d %b %Y at %I:%M%p")}')
 
         await ctx.message.delete()
 
