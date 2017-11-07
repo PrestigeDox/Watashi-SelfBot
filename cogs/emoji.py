e95c62055ff4622f5a735a681266d1b4d715d47b
James <naughtsee@gmail.com>
2017-11-07 00:02:19 -0500

    fix editing in emoji

--- cogs/emoji.py
+++ cogs/emoji.py
@@ -17,77 +17,77 @@ class Emoji:
     @emoji.command()
     async def shrug(self, ctx):
         """ Lenny Shrug """
-        await ctx.message.edit("¯\_(ツ)_/¯")
+        await ctx.message.edit(content="¯\_(ツ)_/¯")
 
     @emoji.command()
     async def face(self, ctx):
         """ Lenny Face """
-        await ctx.message.edit("( ͡° ͜ʖ ͡°)")
+        await ctx.message.edit(content="( ͡° ͜ʖ ͡°)")
 
     @emoji.command()
     async def badman(self, ctx):
         """ Lenny Badman With Guns """
-        await ctx.message.edit("̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿")
+        await ctx.message.edit(content="̿̿ ̿̿ ̿̿ ̿'̿'\̵͇̿̿\з= ( ▀ ͜͞ʖ▀) =ε/̵͇̿̿/’̿’̿ ̿ ̿̿ ̿̿ ̿̿")
 
     @emoji.command(aliases=['guns'])
     async def gun(self, ctx):
         """ Gun Emoji"""
-        await ctx.message.edit("▄︻̷̿┻̿═━一")
+        await ctx.message.edit(content="▄︻̷̿┻̿═━一")
 
     @emoji.command()
     async def ameno(self, ctx):
         """ Lenny Give Emoji """
-        await ctx.message.edit("༼ つ ◕_◕ ༽つ")
+        await ctx.message.edit(content="༼ つ ◕_◕ ༽つ")
 
     @emoji.command()
     async def sunglasses(self, ctx):
         """ Lenny Wearing Sunglasses """
-        await ctx.message.edit("(▀̿Ĺ̯▀̿ ̿)")
+        await ctx.message.edit(content="(▀̿Ĺ̯▀̿ ̿)")
 
     @emoji.command()
     async def eyesworried(self, ctx):
         """ Worried Eyes Emoji """
-        await ctx.message.edit("ಠ_ಠ")
+        await ctx.message.edit(content="ಠ_ಠ")
 
     @emoji.command(aliases=['5dollars', '5dollar', 'money'])
     async def fivedollar(self, ctx):
         """ Five Dollar Bill Emoji """
-        await ctx.message.edit("[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]")
+        await ctx.message.edit(content="[̲̅$̲̅(̲̅5̲̅)̲̅$̲̅]")
 
     @emoji.command()
     async def hiding(self, ctx):
         """  Hiding Lenny Man """
-        await ctx.message.edit("┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴")
+        await ctx.message.edit(content="┬┴┬┴┤ ͜ʖ ͡°) ├┬┴┬┴")
 
     @emoji.command()
     async def boxing(self, ctx):
         """ Boxing Emoji """
-        await ctx.message.edit("(ง'̀-'́)ง")
+        await ctx.message.edit(content="(ง'̀-'́)ง")
 
     @emoji.command()
     async def tableflip(self, ctx):
         """ Lenny Table Flip """
-        await ctx.message.edit("(╯°□°）╯︵ ┻━┻")
+        await ctx.message.edit(content="(╯°□°）╯︵ ┻━┻")
 
     @emoji.command(aliases=['0.o', '0.0', 'o.o'])
     async def weirdeyes(self, ctx):
         """ Weird Eyes Emoji """
-        await ctx.message.edit("◉_◉")
+        await ctx.message.edit(content="◉_◉")
 
     @emoji.command()
     async def tableflip2(self, ctx):
         """ Lenny Flipping 2 Tables at once """
-        await ctx.message.edit("┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻")
+        await ctx.message.edit(content="┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻")
 
     @emoji.command(aliases=['cuteface'])
     async def cute(self, ctx):
         """ Cute Face Emoji """
-        await ctx.message.edit("(｡◕‿◕｡)")
+        await ctx.message.edit(content="(｡◕‿◕｡)")
 
     @emoji.command()
     async def unflip(self, ctx):
         """ Lenny Unflip """
-        await ctx.message.edit("┬─┬﻿ ノ( ゜-゜ノ)")
+        await ctx.message.edit(content="┬─┬﻿ ノ( ゜-゜ノ)")
 
 
 def setup(bot):
